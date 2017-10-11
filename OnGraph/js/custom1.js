a25ba0c7c36aa6f9857e1e6ed7f73c23edb58bfc
Sawan Kumar <sawan.kumar@aurigait.com>
2017-10-11 14:31:16 +0530

    Drag Drop Some part done

--- OnGraph/js/custom1.js
+++ OnGraph/js/custom1.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	a = [];
-
-	adjustChatWindow();
+	b = 0;
+	
 	var initial_chat = JSON.parse(localStorage.getItem('chat_text'));
 	if(initial_chat != undefined) {
 		a = initial_chat;
@@ -20,13 +20,59 @@ $(document).ready(function(){
 		return false;
 	});
 	loadChat(a);
-	height = $('.chat')[0].scrollHeight;
-	scrollToChatBottom(height);
+	openChatWindow();
+	minimize();
+	maximize();
+	close();
+	doubleClickMaximizePopup();
 });
+
+function doubleClickMaximizePopup() {
+	$('.top-bar').on('dblclick', function(){
+		$('.chat-container').toggleClass('maximize');
+		$('.chat-container').removeClass('minimize');
+		adjustChatWindow();
+	});
+}
+
+function openChatWindow() {
+	$('.open-chat-window').on('click', function(){
+		$('.chat-container').show(500, function(){
+			adjustChatWindow();
+			height = $('.chat')[0].scrollHeight;
+			scrollToChatBottom(height);
+		});
+		$('.overlay').show(500);
+		//doFirst();
+	});
+}
+
+function minimize() {
+	$('#minimize').on('click', function(){
+		$('.chat-container').toggleClass('minimize');
+		$('.chat-container').removeClass('maximize');
+		adjustChatWindow();
+	});
+}
+function maximize() {
+	$('#maximize').on('click', function(){
+		$('.chat-container').toggleClass('maximize');
+		$('.chat-container').removeClass('minimize');
+		adjustChatWindow();
+	});
+}
+function close() {
+	$('#close').on('click', function(){
+		$('.chat-container').hide(500);
+		$('.overlay').hide(500);
+		$('.chat-container').removeClass('maximize');
+		$('.chat-container').removeClass('minimize');
+	});
+}
 
 function adjustChatWindow() {
 	var textarea_height = $('.messageTextarea').outerHeight();
-	var chat_height = $('.chat-container').innerHeight() - textarea_height;
+	var chat_height = $('.chat-wrapper').height() - textarea_height;
 	$('.chat').innerHeight(chat_height);
 }
 
@@ -60,4 +106,45 @@ function scrollToChatBottom(height) {
 	$('.chat').animate({
         scrollTop: height
     }, 1000);
+}
+
+function dragStart(event) {
+    //event.dataTransfer.setData("Text", event.target.id);
+    var elem = event.target;
+    var top_pos = parseInt($(elem).offset().top);
+    var left_pos = parseInt($(elem).offset().left);
+    console.log('DragStart');
+    $(elem).css({
+    	'position': 'absolute'
+    });
+}
+
+function dragging(event) {
+    //document.getElementById("demo").innerHTML = "The p element is being dragged";
+    var elem = event.target;
+    var top_pos = parseInt($(elem).offset().top);
+    var left_pos = parseInt($(elem).offset().left);
+    console.log(top_pos, left_pos);
+    $(elem).css({
+    	'top': top_pos,
+    	'left': left_pos
+    });
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    /*var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));*/
+    //document.getElementById("demo").innerHTML = "The p element was dropped";
+    var elem = event.target;
+    var top_pos = parseInt($(elem).offset().top);
+    var left_pos = parseInt($(elem).offset().left);
+    $(elem).css({
+    	'top': top_pos,
+    	'left': left_pos
+    });
 }
